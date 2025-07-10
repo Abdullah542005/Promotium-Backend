5bd715ed73e127910d10e819f9bb0ba60bdf7d2b
Abdullah542005 <abdullahkhan542005@gmail.com>
2025-07-10 13:46:43 +0500

    Database Models Added

---
+++ UserManagementServer/model/dbModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.DB_CONNECTIONSTRING, { dbName: "Promotium" });
+
+const userSchema  = new mongoose.Schema({
+    _id:{
+         type:String,
+         unique:true,
+         require:true
+    },
+     address:{
+         type:String,
+         unique:true,
+         require:true
+     },
+     fullName:String,
+     username:{
+         type:String,
+         unique:true,
+         require:true
+     },
+     bio:String,
+     country:String,
+     pfp :String,
+     X:{username:String, token:String},
+     facebook:{username:String, token:String},
+     followers:[String],
+     follows:[String],
+     posts:[String],
+     interactions:[{postID:String, interactionID:String}],
+     isValidator:Boolean
+})
+
+modulde.export  = mongoose.model('User',userSchema);
