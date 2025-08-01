5fcd8a96adc5f92fbbd8b57f65f750c8ce327da4
Abdullah542005 <abdullahkhan542005@gmail.com>
2025-08-01 11:58:02 +0500

    Fixes Made

--- UserManagementServer/controllers/createAccount.js
+++ UserManagementServer/controllers/createAccount.js
@@ -1,4 +1,18 @@
 const users = require('../model/dbModel');
+const {nanoid} = require("nanoid")
+
+/* user = {
+     address:{
+         type:String,
+         unique:true,
+         require:true
+     },
+     fullName:String,
+     username:String
+     pfp :String,
+     X:{username:String, token:String},
+     facebook:{username:String, token:String},
+} */
 
 exports.createAccount = async (req, res) => {
 
@@ -11,7 +25,14 @@ exports.createAccount = async (req, res) => {
       return res.status(400).json({ message: "User with same credentials exists" });
     }
 
-    const newUser = await users.create(user);
+    await users.insertOne({
+      _id:nanoid(4),
+      address:user.address,
+      userName:user.userName,
+      pfp:user.pfp,
+      X:{username:user.X.username,token:user.X.token},
+      facebook:{username:user.facebook.username,token:user.facebook.token}
+    }).exec();
 
     return res.status(201).json({ message: "User created", user: newUser });
   } catch (err) {
