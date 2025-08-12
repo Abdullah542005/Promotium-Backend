08345b182ae43365edb981f931c8947ba96c3d20
Abdullah542005 <abdullahkhan542005@gmail.com>
2025-08-12 12:00:41 +0500

    Contract Post B Changed

--- PostAndInteractionServer/models/contractPostB.js
+++ PostAndInteractionServer/models/contractPostB.js
@@ -6,7 +6,7 @@ const provider  = new ethers.JsonRpcProvider('https://rpc.test2.btcs.network/', 
     name:"Core Blockchain Testnet2",
     chainId:1114
 })
-const contract =  new ethers.Contract('0x8b1443B4Fc488a08e9D7083c18576D5ad900887f',contractBAbi, new ethers.Wallet(process.env.PRIVATEKEY,provider));
+const contract =  new ethers.Contract('0x7293c154e9Ab2cd9C209f4E9d26015d1536dD6F2',contractBAbi, new ethers.Wallet(process.env.PRIVATEKEY,provider));
 
 module.exports = contract;
 
