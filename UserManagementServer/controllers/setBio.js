e80ad55bd5852b8f3f4d094679bfa7753491a59d
ifaixal <fai.iqbal124@gmail.com>
2025-08-14 12:13:55 +0500

    Set Bio Fix

--- UserManagementServer/controllers/setBio.js
+++ UserManagementServer/controllers/setBio.js
@@ -1,11 +1,11 @@
 const UserModel  = require("../model/dbModel")
 
 
-exports.setBio = (req,res) => { 
+exports.setBio = async (req,res) => { 
      try{ 
         const {userAddress,bio}  = req.body
-        if(!userAddress || !bio) return res.stats(400).json({message:"Insufficient Params"})
-        UserModel.updateOne({address:userAddress}, {$set:{bio:bio}})
+        if(!userAddress || !bio) return res.status(400).json({message:"Insufficient Params"})
+        await UserModel.updateOne({address:userAddress}, {$set:{bio:bio}})
         return res.json({message:"Bio Set Successfully"})    
      }catch(error){ 
         res.status(401).json({message:"Server Encountered An Error"})
